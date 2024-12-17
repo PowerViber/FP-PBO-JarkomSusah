54f81896ac4a3ef9184d3853dcc2bd9ae426b22f
PowerViber <arianza429@gmail.com>
2024-12-17 15:24:40 +0700

    add stage2 rand doors to win

--- Sepuluh Nopember’s Adventure/Sepuluh Nopember’s Adventure/Second.cs
+++ Sepuluh Nopember’s Adventure/Sepuluh Nopember’s Adventure/Second.cs
@@ -35,6 +35,17 @@ namespace Sepuluh_Nopember_s_Adventure
         //top area -> invis player
         private List<PictureBox> _topPictureBoxes;
 
+        // Doors
+        private PictureBox door1;
+        private PictureBox door2;
+        private PictureBox door3;
+        private List<PictureBox> _doors;
+
+        private PictureBox winningDoor; // Store the current winning door
+        private Random random = new Random(); // Random number generator
+        private bool doorInteractionTriggered = false; // Flag to prevent multiple triggers
+
+
         public Second()
         {
             Init();
@@ -53,14 +64,31 @@ namespace Sepuluh_Nopember_s_Adventure
             //-------- BOUNDARIES LIST ----------//
             _boundaries = new List<PictureBox>();
 
-            //AddBoundary(new Rectangle(0, 620, 320, 80));
+            //wall jembatan
+            AddBoundary(new Rectangle(65, 500, 10, 350)); //bawah kiri
+            AddBoundary(new Rectangle(490, 0, 10, 490)); //atas kiri
+            AddBoundary(new Rectangle(660, 0, 10, 640)); //atas kanan
+            AddBoundary(new Rectangle(65, 500, 435, 10)); //tengah atas
+            AddBoundary(new Rectangle(250, 650, 435, 10)); //tengah bawah
+            AddBoundary(new Rectangle(240, 650, 10, 180)); //bawah kanan
+
+            //wall log
+            AddBoundary(new Rectangle(240, 830, 550, 10)); //bawah kanan
+
+            //pojok kiri bawah
+            AddBoundary(new Rectangle(0, 775, 60, 10));
+
+            //pembedaan pintu
+            AddBoundary(new Rectangle(547, 0, 2, 90)); //kiri
+            AddBoundary(new Rectangle(607, 0, 2, 90)); //kanan
+
 
             //------------------------------------//
 
             //------------------- TOP LIST --------------------//
             _topPictureBoxes = new List<PictureBox>();
 
-            //AddTop(new Rectangle(664, 700, 117, 130));
+            AddTop(new Rectangle(490, 0, 180, 70));
 
             foreach (var top in _topPictureBoxes)
             {
@@ -79,7 +107,7 @@ namespace Sepuluh_Nopember_s_Adventure
             {
                 InteractionAction = () =>
                 {
-                    MessageBox.Show("halo");
+                    npc1.ProvideQuest();
                 }
             };
             this.Controls.Add(npc1_pbox);
@@ -101,6 +129,21 @@ namespace Sepuluh_Nopember_s_Adventure
             }
             //---------------------------//
 
+            // Doors Setup
+            door1 = CreateDoor(new Point(508, 0)); // Left Door
+            door2 = CreateDoor(new Point(568, 0)); // Middle Door
+            door3 = CreateDoor(new Point(627, 0)); // Right Door
+
+            _doors = new List<PictureBox> { door1, door2, door3 };
+
+            // Add doors to form
+            this.Controls.Add(door1);
+            this.Controls.Add(door2);
+            this.Controls.Add(door3);
+
+            // Randomize winning door
+            RandomizeWinningDoor();
+
             //player
             _player = new Player(new Point(PlayerX, PlayerY));
             this.Controls.Add(_player.GetPictureBox());
@@ -113,6 +156,7 @@ namespace Sepuluh_Nopember_s_Adventure
                 bool canPassBoundary = true;
 
                 _player.Walk(this.ClientSize, _npcPictureBoxes, restrictedArea, canPassBoundary, _boundaries);
+                CheckDoorCollision();
             };
             _animationTimer.Start();
 
@@ -149,7 +193,7 @@ namespace Sepuluh_Nopember_s_Adventure
             {
                 Location = new Point(bounds.X, bounds.Y),
                 Size = new Size(bounds.Width, bounds.Height),
-                BackColor = Color.Transparent, // Replace with transparent if using actual map graphics
+                BackColor = Color.Transparent, 
             };
             this.Controls.Add(boundary);
             _boundaries.Add(boundary);
@@ -172,6 +216,69 @@ namespace Sepuluh_Nopember_s_Adventure
 
             this.Controls.Add(top);
             _topPictureBoxes.Add(top);
+        }
+
+        private PictureBox CreateDoor(Point location)
+        {
+            return new PictureBox
+            {
+                Location = location,
+                Size = new Size(25, 40),
+                BackColor = Color.Transparent, 
+                BorderStyle = BorderStyle.Fixed3D
+            };
+        }
+
+        private void RandomizeWinningDoor()
+        {
+            int randomIndex = random.Next(0, _doors.Count);
+            winningDoor = _doors[randomIndex];
+            //winningDoor.BackColor = Color.Green; 
+        }
+
+        private void CheckDoorCollision()
+        {
+            if (doorInteractionTriggered) return; 
+
+            Rectangle playerBounds = _player.GetPictureBox().Bounds;
+
+            foreach (var door in _doors)
+            {
+                if (playerBounds.IntersectsWith(door.Bounds))
+                {
+                    doorInteractionTriggered = true; 
+
+                    if (door == winningDoor)
+                    {
+                        ShowWinMessage();
+                    }
+                    else
+                    {
+                        ShowLoseMessage();
+                    }
+                    break; 
+                }
+            }
+        }
+
+
+        private void ShowLoseMessage()
+        {
+            MessageBox.Show("Oops! Wrong door. Try again.", "You Lost!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            ResetGame();
+        }
+
+        private void ShowWinMessage()
+        {
+            MessageBox.Show("Congratulations! You've chosen the correct door!", "You Win!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Close();
+        }
+
+        private void ResetGame()
+        {
+            _player.GetPictureBox().Location = new Point(568, 300);
+            RandomizeWinningDoor();
+            doorInteractionTriggered = false;
         }
     }
 }
